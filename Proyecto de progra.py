dec237251c51fc6e50eca82c8894b13ec88112ab
SebasCoto <secogonzalez2005@gmail.com>
2023-03-12 21:34:14 -1200

    Primer commit

---
+++ Proyecto de progra.py
@@ -0,0 +1,46 @@
+import getpass
+def usuarioNuevo():
+    intentos=0
+    max_intentos=3
+    while intentos<3 :
+    
+        cedula = input("Ingrese su cédula de identidad (debe tener 9 dígitos): ")
+        if len(cedula) == 9:
+             print("Cédula válida.")
+             break
+        else:
+            intentos+=1
+            print("Intente de nuevo\nError: La cedual debe tener (9 digitos)")
+    else:
+        print("Usted excedió el numero de intentos ")
+    nombre=input("Ingrese su nombre: \n")
+    while True:
+        pin=getpass.getpass("Ingrese el PIN que desea para su cuenta bancaria:\n")
+        if len(pin)==4:
+            print("PIN valido")
+            break
+        else:
+            print("Su PIN debe de ser de 4 digitos,intente de nuevo")
+    print("Ingrese nuevamente el PIN para auntenticar")
+    while True:
+        aunte_Pin=getpass.getpass("Ingrese el PIN para auntenticar:\n")
+        if aunte_Pin==pin:
+            print("Se auntentico correctamente")
+            break
+        else:
+            print("El PIN no es igual,Intente de nuevo")
+    
+    
+
+
+
+
+
+
+
+
+
+#----------PROGRAMA PRINCIPAL-------------
+print("Bienvenido a nuestro banco, que desea?")
+menu=input("1-Registrar Usuario nuevo\n2-Usuario Registrado\n3-Configuracion Avanzada\n4-Salir\n")
+print(usuarioNuevo())
